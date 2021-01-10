b6ac783da361e665b4186961d915aad93ad3e19b
ayoung0073 <ayong0310@naver.com>
2021-01-10 23:14:41 +0900

    Fix - logging 수정

--- src/main/java/com/mayko/ewhaplate/api/ControllerAdvice.java
+++ src/main/java/com/mayko/ewhaplate/api/ControllerAdvice.java
@@ -1,14 +1,17 @@
 package com.mayko.ewhaplate.api;
 
 import com.mayko.ewhaplate.dto.response.ExceptionDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerAdvice {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass()); // logger.debug 안 찍힘
     @ExceptionHandler(value = Exception.class)
     public ExceptionDto handleException(Exception e) {
-        e.printStackTrace();
+        logger.info("EXCEPTION ===> " + e.getMessage());
         return new ExceptionDto(e.getMessage());
     }
 }
