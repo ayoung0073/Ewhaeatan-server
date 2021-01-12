fb5e0032ee93b190028fedb227fca6d98899580b
ayoung0073 <ayong0310@naver.com>
2021-01-12 21:33:32 +0900

    Feat - logging 추가

---
+++ src/main/java/com/mayko/ewhaplate/aop/LoggingAspect.java
@@ -0,0 +1,35 @@
+package com.mayko.ewhaplate.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass()); // logger.debug 안 찍힘
+
+    // controller 패키지에서 *Controller 클래스에 있는 메서드 중 파라미터 0개인 메서드만
+    // service 패키지에서 *Service 클래스에 있는 메서드 중 파라미터 0개인 메서드만
+    @Around("execution(* com.mayko.ewhaplate..api.*Controller.*(..))")
+    public Object printLog(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        String type = "";
+        String name = joinPoint.getSignature().getDeclaringTypeName();
+
+        if (name.contains("Controller")) {
+            type = "Controller - '";
+        }
+
+        logger.info(type + name + "." + joinPoint.getSignature().getName() + "'");
+        logger.info(Arrays.toString(joinPoint.getArgs()));
+        // getName - 메서드 이름
+        return joinPoint.proceed();
+    }
+}
--- src/test/java/com/mayko/ewhaplate/service/FoodServiceTest.java
+++ src/test/java/com/mayko/ewhaplate/service/FoodServiceTest.java
@@ -7,58 +7,83 @@ import com.mayko.ewhaplate.repository.FoodRepository;
 import com.mayko.ewhaplate.repository.MenuRepository;
 import org.junit.Before;
 import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.Rule;
+import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.junit.Assert.*;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
-@Ignore
 public class FoodServiceTest {
 
     @Autowired
     private FoodService foodService;
 
-    @MockBean
+    //@MockBean
+    @Autowired
     private FoodRepository foodRepository;
 
-    @MockBean
-    private MenuRepository menuRepository;
+//    @MockBean
+//    private MenuRepository menuRepository;
 
-    Food food;
-    FoodRandomRequestDto foodRequestDto;
+//    @Before
+//    public void setUp(){
+//    }
 
-    @Before
-    public void setUp(){
-        FoodRequestDto dto = new FoodRequestDto("낭만식탁", "일식", "ㅁㄹ", "030", "정문", "");
-        FoodRequestDto dto2 = new FoodRequestDto("서브웨이", "일식", "ㅁㄹ", "030", "정문", "");
-        FoodRequestDto dto3 = new FoodRequestDto("마린파스타", "일식", "ㅁㄹ", "030", "정문", "");
-        FoodRequestDto dto4 = new FoodRequestDto("스탠바이키친", "일식", "ㅁㄹ", "030", "정문", "");
-
-        List<Food> foodList = new ArrayList<>();
-        foodList.add(new Food(dto));
-        foodList.add(new Food(dto2));
-        foodList.add(new Food(dto3));
-        foodList.add(new Food(dto4));
-
-        foodRepository.saveAll(foodList);
-    }
-
-    @Test
+    @Test // org.junit 이 아니라 org.junit.jupiter.api.Test; 를 import 해야 test 통과
+    @Transactional
     public void 랜덤_테스트_성공케이스(){
 
+//        FoodRequestDto dto = new FoodRequestDto("낭만식탁", "일식", "ㅁㄹ", "030", "정문", "");
+//        FoodRequestDto dto2 = new FoodRequestDto("서브웨이", "중식", "ㅁㄹ", "030", "정문", "");
+//        FoodRequestDto dto3 = new FoodRequestDto("마린파스타", "일식", "ㅁㄹ", "030", "후문", "");
+//        FoodRequestDto dto4 = new FoodRequestDto("스탠바이키친", "일식", "ㅁㄹ", "030", "신촌", "");
+//
+//        List<Food> foodList = new ArrayList<>();
+//        foodList.add(new Food(dto));
+//        foodList.add(new Food(dto2));
+//        foodList.add(new Food(dto3));
+//        foodList.add(new Food(dto4));
+//
+//        foodRepository.saveAll(foodList);
         List<String> list = new ArrayList<>();
+
         list.add("일식");
-        foodRequestDto = new FoodRandomRequestDto(list, "정문");
+
+        FoodRandomRequestDto foodRequestDto = new FoodRandomRequestDto(list, "정문");
+
         Food food = foodService.getRandomFood(foodRequestDto);
-        assertNotNull(food);
+        //assertThat("정문", is(food.getEwhaType()));
+        assertEquals("정문", food.getEwhaType());
+        //assertNotNull(food);
+    }
+
+//    @Rule
+//    public final ExpectedException exception = ExpectedException.none();
+
+    @Test
+    @Transactional
+    public void 랜덤_테스트_예외케이스(){
+        List<String> list = new ArrayList<>();
+
+        list.add("일식");
+
+        FoodRandomRequestDto foodRequestDto = new FoodRandomRequestDto(list, "정문");
+
+        Food food = foodService.getRandomFood(foodRequestDto);
+        //exception.expect(IllegalArgumentException.class);
+        assertNotEquals("후문", food.getEwhaType());
     }
 }
